3b2486c50dac402b328c04968381de2f9671f8fa
aveltras <romain.viallard@outlook.fr>
2021-02-07 18:43:23 +0100

    Init

---
+++ shell.nix
@@ -0,0 +1,27 @@
+{ haskellNix ? import (builtins.fetchTarball "https://github.com/input-output-hk/haskell.nix/archive/2e38dc1cd7ffc1e473e95e998c22ff031ea93f79.tar.gz") {}
+, nixpkgsSrc ? haskellNix.sources.nixpkgs-2009
+, nixpkgsArgs ? haskellNix.nixpkgsArgs
+, pkgs ? import nixpkgsSrc nixpkgsArgs
+}:
+
+(pkgs.haskell-nix.project {
+  compiler-nix-name = "ghc8103";
+  # index-state = "2021-01-19T00:00:00Z";
+  src = pkgs.haskell-nix.haskellLib.cleanGit {
+    name = "lucid-carbon";
+    src = ./.;
+  };
+}).shellFor {
+  packages = ps: with ps; [
+    lucid-carbon
+  ];
+  
+  withHoogle = false;
+  exactDeps = true;
+
+  tools = {
+    cabal-install = "latest";
+    haskell-language-server = "latest";
+    ghcid = "latest";
+  };
+}
---
+++ src/Lucid/Carbon.hs
@@ -0,0 +1,326 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module Lucid.Carbon where
+
+import Lucid.Base (Term (term))
+
+bxAccordion_ :: Term arg result => arg -> result
+bxAccordion_ = term "bx-accordion"
+
+bxAccordionItem_ :: Term arg result => arg -> result
+bxAccordionItem_ = term "bx-accordion-item"
+
+bxBreadcrumb_ :: Term arg result => arg -> result
+bxBreadcrumb_ = term "bx-breadcrumb"
+
+bxBreadcrumbItem_ :: Term arg result => arg -> result
+bxBreadcrumbItem_ = term "bx-breadcrumb-item"
+
+bxBreadcrumbLink_ :: Term arg result => arg -> result
+bxBreadcrumbLink_ = term "bx-breadcrumb-link"
+
+bxButton_ :: Term arg result => arg -> result
+bxButton_ = term "bx-btn"
+
+bxCheckbox_ :: Term arg result => arg -> result
+bxCheckbox_ = term "bx-checkbox"
+
+bxCodeSnippet_ :: Term arg result => arg -> result
+bxCodeSnippet_ = term "bx-code-snippet"
+
+bxComboBox_ :: Term arg result => arg -> result
+bxComboBox_ = term "bx-combo-box"
+
+bxComboBoxItem_ :: Term arg result => arg -> result
+bxComboBoxItem_ = term "bx-combo-box-item"
+
+bxContentSwitcher_ :: Term arg result => arg -> result
+bxContentSwitcher_ = term "bx-content-switcher"
+
+bxContentSwitcherItem_ :: Term arg result => arg -> result
+bxContentSwitcherItem_ = term "bx-content-switcher-item"
+
+bxCopyButton_ :: Term arg result => arg -> result
+bxCopyButton_ = term "bx-copy-button"
+
+bxDataTable_ :: Term arg result => arg -> result
+bxDataTable_ = term "bx-data-table"
+
+bxTable_ :: Term arg result => arg -> result
+bxTable_ = term "bx-table"
+
+bxTableHead_ :: Term arg result => arg -> result
+bxTableHead_ = term "bx-table-head"
+
+bxTableHeaderRow_ :: Term arg result => arg -> result
+bxTableHeaderRow_ = term "bx-table-header-row"
+
+bxTableHeaderCell_ :: Term arg result => arg -> result
+bxTableHeaderCell_ = term "bx-table-header-cell"
+
+bxTableBody_ :: Term arg result => arg -> result
+bxTableBody_ = term "bx-table-body"
+
+bxTableRow_ :: Term arg result => arg -> result
+bxTableRow_ = term "bx-table-row"
+
+bxTableCell_ :: Term arg result => arg -> result
+bxTableCell_ = term "bx-table-cell"
+
+bxTableToolbar_ :: Term arg result => arg -> result
+bxTableToolbar_ = term "bx-table-toolbar"
+
+bxTableBatchActions_ :: Term arg result => arg -> result
+bxTableBatchActions_ = term "bx-table-batch-actions"
+
+bxTableToolbarContent_ :: Term arg result => arg -> result
+bxTableToolbarContent_ = term "bx-table-toolbar-content"
+
+bxTableToolbarSearch_ :: Term arg result => arg -> result
+bxTableToolbarSearch_ = term "bx-table-toolbar-search"
+
+bxDatePicker_ :: Term arg result => arg -> result
+bxDatePicker_ = term "bx-date-picker"
+
+bxDatePickerInput_ :: Term arg result => arg -> result
+bxDatePickerInput_ = term "bx-date-picker-input"
+
+bxDropdown_ :: Term arg result => arg -> result
+bxDropdown_ = term "bx-dropdown"
+
+bxDropdownItem_ :: Term arg result => arg -> result
+bxDropdownItem_ = term "bx-dropdown-item"
+
+bxFileUploader_ :: Term arg result => arg -> result
+bxFileUploader_ = term "bx-file-uploader"
+
+bxFileDropContainer_ :: Term arg result => arg -> result
+bxFileDropContainer_ = term "bx-file-drop-container"
+
+bxFileUploaderItem_ :: Term arg result => arg -> result
+bxFileUploaderItem_ = term "bx-file-uploader-item"
+
+bxFormItem_ :: Term arg result => arg -> result
+bxFormItem_ = term "bx-form-item"
+
+bxInlineLoading_ :: Term arg result => arg -> result
+bxInlineLoading_ = term "bx-inline-loading"
+
+bxInput_ :: Term arg result => arg -> result
+bxInput_ = term "bx-input"
+
+bxLink_ :: Term arg result => arg -> result
+bxLink_ = term "bx-link"
+
+bxOrderedList_ :: Term arg result => arg -> result
+bxOrderedList_ = term "bx-ordered-list"
+
+bxUnorderedList_ :: Term arg result => arg -> result
+bxUnorderedList_ = term "bx-unordered-list"
+
+bxListItem_ :: Term arg result => arg -> result
+bxListItem_ = term "bx-list-item"
+
+bxLoading_ :: Term arg result => arg -> result
+bxLoading_ = term "bx-loading"
+
+bxModal_ :: Term arg result => arg -> result
+bxModal_ = term "bx-modal"
+
+bxModalHeader_ :: Term arg result => arg -> result
+bxModalHeader_ = term "bx-modal-header"
+
+bxModalCloseButton_ :: Term arg result => arg -> result
+bxModalCloseButton_ = term "bx-modal-close-button"
+
+bxModalLabel_ :: Term arg result => arg -> result
+bxModalLabel_ = term "bx-modal-label"
+
+bxModalHeading_ :: Term arg result => arg -> result
+bxModalHeading_ = term "bx-modal-heading"
+
+bxModalBody_ :: Term arg result => arg -> result
+bxModalBody_ = term "bx-modal-body"
+
+bxModalFooter_ :: Term arg result => arg -> result
+bxModalFooter_ = term "bx-modal-footer"
+
+bxMultiSelect_ :: Term arg result => arg -> result
+bxMultiSelect_ = term "bx-multi-select"
+
+bxMultiSelectItem_ :: Term arg result => arg -> result
+bxMultiSelectItem_ = term "bx-multi-select-item"
+
+bxInlineNotification_ :: Term arg result => arg -> result
+bxInlineNotification_ = term "bx-inline-notification"
+
+bxToastNotification_ :: Term arg result => arg -> result
+bxToastNotification_ = term "bx-toast-notification"
+
+bxNumberInput_ :: Term arg result => arg -> result
+bxNumberInput_ = term "bx-number-input"
+
+bxOverflowMenu_ :: Term arg result => arg -> result
+bxOverflowMenu_ = term "bx-overflow-menu"
+
+bxOverflowMenuBody_ :: Term arg result => arg -> result
+bxOverflowMenuBody_ = term "bx-overflow-menu-body"
+
+bxOverflowMenuItem_ :: Term arg result => arg -> result
+bxOverflowMenuItem_ = term "bx-overflow-menu-item"
+
+bxPagination_ :: Term arg result => arg -> result
+bxPagination_ = term "bx-pagination"
+
+bxPageSizesSelect_ :: Term arg result => arg -> result
+bxPageSizesSelect_ = term "bx-page-sizes-select"
+
+bxPagesSelect_ :: Term arg result => arg -> result
+bxPagesSelect_ = term "bx-pages-select"
+
+bxProgressIndicator_ :: Term arg result => arg -> result
+bxProgressIndicator_ = term "bx-progress-indicator"
+
+bxProgressStep_ :: Term arg result => arg -> result
+bxProgressStep_ = term "bx-progress-step"
+
+bxRadioButton_ :: Term arg result => arg -> result
+bxRadioButton_ = term "bx-radio-button"
+
+bxRadioButtonGroup_ :: Term arg result => arg -> result
+bxRadioButtonGroup_ = term "bx-radio-button-group"
+
+bxSearch_ :: Term arg result => arg -> result
+bxSearch_ = term "bx-search"
+
+bxSelect_ :: Term arg result => arg -> result
+bxSelect_ = term "bx-select"
+
+bxSelectItem_ :: Term arg result => arg -> result
+bxSelectItem_ = term "bx-select-item"
+
+bxSelectItemGroup_ :: Term arg result => arg -> result
+bxSelectItemGroup_ = term "bx-select-item-group"
+
+bxSkeletonPlaceholder_ :: Term arg result => arg -> result
+bxSkeletonPlaceholder_ = term "bx-skeleton-placeholder"
+
+bxSkeletonText_ :: Term arg result => arg -> result
+bxSkeletonText_ = term "bx-skeleton-text"
+
+bxSkipToContent_ :: Term arg result => arg -> result
+bxSkipToContent_ = term "bx-skip-to-content"
+
+bxSlider_ :: Term arg result => arg -> result
+bxSlider_ = term "bx-slider"
+
+bxSliderInput_ :: Term arg result => arg -> result
+bxSliderInput_ = term "bx-slider-input"
+
+bxStructuredList_ :: Term arg result => arg -> result
+bxStructuredList_ = term "bx-structured-list"
+
+bxStructuredListHead_ :: Term arg result => arg -> result
+bxStructuredListHead_ = term "bx-structured-list-head"
+
+bxStructuredListHeaderRow_ :: Term arg result => arg -> result
+bxStructuredListHeaderRow_ = term "bx-structured-list-header-row"
+
+bxStructuredListHeaderCell_ :: Term arg result => arg -> result
+bxStructuredListHeaderCell_ = term "bx-structured-list-header-cell"
+
+bxStructuredListBody_ :: Term arg result => arg -> result
+bxStructuredListBody_ = term "bx-structured-list-body"
+
+bxStructuredListRow_ :: Term arg result => arg -> result
+bxStructuredListRow_ = term "bx-structured-list-row"
+
+bxStructuredListCell_ :: Term arg result => arg -> result
+bxStructuredListCell_ = term "bx-structured-list-cell"
+
+bxTabs_ :: Term arg result => arg -> result
+bxTabs_ = term "bx-tabs"
+
+bxTab_ :: Term arg result => arg -> result
+bxTab_ = term "bx-tab"
+
+bxTag_ :: Term arg result => arg -> result
+bxTag_ = term "bx-tag"
+
+bxFilterTag_ :: Term arg result => arg -> result
+bxFilterTag_ = term "bx-filter-tag"
+
+bxTextarea_ :: Term arg result => arg -> result
+bxTextarea_ = term "bx-textarea"
+
+bxTile_ :: Term arg result => arg -> result
+bxTile_ = term "bx-tile"
+
+bxClickableTile_ :: Term arg result => arg -> result
+bxClickableTile_ = term "bx-clickable-tile"
+
+bxExpandableTile_ :: Term arg result => arg -> result
+bxExpandableTile_ = term "bx-expandable-tile"
+
+bxSelectableTile_ :: Term arg result => arg -> result
+bxSelectableTile_ = term "bx-selectable-tile"
+
+bxRadioTile_ :: Term arg result => arg -> result
+bxRadioTile_ = term "bx-radio-tile"
+
+bxTileGroup_ :: Term arg result => arg -> result
+bxTileGroup_ = term "bx-tile-group"
+
+bxToggle_ :: Term arg result => arg -> result
+bxToggle_ = term "bx-toggle"
+
+bxTooltip_ :: Term arg result => arg -> result
+bxTooltip_ = term "bx-tooltip"
+
+bxTooltipBody_ :: Term arg result => arg -> result
+bxTooltipBody_ = term "bx-tooltip-body"
+
+bxTooltipDefinition_ :: Term arg result => arg -> result
+bxTooltipDefinition_ = term "bx-tooltip-definition"
+
+bxTooltipFooter_ :: Term arg result => arg -> result
+bxTooltipFooter_ = term "bx-tooltip-footer"
+
+bxTooltipIcon_ :: Term arg result => arg -> result
+bxTooltipIcon_ = term "bx-tooltip-icon"
+
+bxHeader_ :: Term arg result => arg -> result
+bxHeader_ = term "bx-header"
+
+bxHeaderNav_ :: Term arg result => arg -> result
+bxHeaderNav_ = term "bx-header-nav"
+
+bxHeaderNavItem_ :: Term arg result => arg -> result
+bxHeaderNavItem_ = term "bx-header-nav-item"
+
+bxHeaderName_ :: Term arg result => arg -> result
+bxHeaderName_ = term "bx-header-name"
+
+bxHeaderMenu_ :: Term arg result => arg -> result
+bxHeaderMenu_ = term "bx-header-menu"
+
+bxHeaderMenuItem_ :: Term arg result => arg -> result
+bxHeaderMenuItem_ = term "bx-header-menu-item"
+
+bxHeaderMenuButton_ :: Term arg result => arg -> result
+bxHeaderMenuButton_ = term "bx-header-menu-button"
+
+bxSideNav_ :: Term arg result => arg -> result
+bxSideNav_ = term "bx-side-nav"
+
+bxSideNavMenu_ :: Term arg result => arg -> result
+bxSideNavMenu_ = term "bx-side-nav-menu"
+
+bxSideNavMenuItem_ :: Term arg result => arg -> result
+bxSideNavMenuItem_ = term "bx-side-menu-item"
+
+bxSideNavLink_ :: Term arg result => arg -> result
+bxSideNavLink_ = term "bx-side-nav-link"
+
+bxSideNavItems_ :: Term arg result => arg -> result
+bxSideNavItems_ = term "bx-side-nav-items"
